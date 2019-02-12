dfe8ff16be0229d5c95537e719fc349b9e3ff32d
sshodges <sshodges@gmail.com>
2019-02-12 14:36:46 +1100

    update sign up email

--- server.js
+++ server.js
@@ -53,6 +53,181 @@ app.post('/v1/users', function(req, res) {
 
     nodemailer.createTestAccount((err, account) => {
 
+      var text = `
+      <!DOCTYPE html>
+      <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+      <head>
+          <title></title>
+          <!--[if !mso]><!-- -->
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <!--<![endif]-->
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <style type="text/css">
+              #outlook a {
+                  padding: 0;
+              }
+
+              .ReadMsgBody {
+                  width: 100%;
+              }
+
+              .ExternalClass {
+                  width: 100%;
+              }
+
+              .ExternalClass * {
+                  line-height: 100%;
+              }
+
+              body {
+                  margin: 0;
+                  padding: 0;
+                  -webkit-text-size-adjust: 100%;
+                  -ms-text-size-adjust: 100%;
+              }
+
+              table,
+              td {
+                  border-collapse: collapse;
+                  mso-table-lspace: 0pt;
+                  mso-table-rspace: 0pt;
+              }
+
+              img {
+                  border: 0;
+                  height: auto;
+                  line-height: 100%;
+                  outline: none;
+                  text-decoration: none;
+                  -ms-interpolation-mode: bicubic;
+              }
+
+              p {
+                  display: block;
+                  margin: 13px 0;
+              }
+          </style>
+          <!--[if !mso]><!-->
+          <style type="text/css">
+              @media only screen and (max-width:480px) {
+                  @-ms-viewport {
+                      width: 320px;
+                  }
+                  @viewport {
+                      width: 320px;
+                  }
+              }
+          </style>
+          <!--<![endif]-->
+          <!--[if mso]><xml>  <o:OfficeDocumentSettings>    <o:AllowPNG/>    <o:PixelsPerInch>96</o:PixelsPerInch>  </o:OfficeDocumentSettings></xml><![endif]-->
+          <!--[if lte mso 11]><style type="text/css">  .outlook-group-fix {    width:100% !important;  }</style><![endif]-->
+          <!--[if !mso]><!-->
+          <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+          <style type="text/css">
+              @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+          </style>
+          <!--<![endif]-->
+          <style type="text/css">
+              @media only screen and (min-width:480px) {
+                  .mj-column-per-100 {
+                      width: 100%!important;
+                  }
+              }
+          </style>
+      </head>
+
+      <body style="background: #FFFFFF;">
+          <div class="mj-container" style="background-color:#FFFFFF;">
+              <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">        <tr>          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">      <![endif]-->
+              <div style="margin:0px auto;max-width:600px;">
+                  <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
+                      <tbody>
+                          <tr>
+                              <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:9px 0px 9px 0px;">
+                                  <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:600px;">      <![endif]-->
+                                  <div class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                                      <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                                          <tbody>
+                                              <tr>
+                                                  <td style="word-wrap:break-word;font-size:0px;padding:18px 6px 0px 6px;" align="left">
+                                                      <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="left" border="0">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td style="width:354px;">
+                                                                      <a href="https://revisioncheck.com" target="_blank"><img alt="" height="auto" src="https://revisioncheck.com/assets/img/logo.png" style="border:none;border-radius:0px;display:block;font-size:13px;outline:none;text-decoration:none;width:100%;height:auto;" width="354"></a>
+                                                                  </td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </div>
+                                  <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+                              </td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+              <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+              <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">        <tr>          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">      <![endif]-->
+              <div style="margin:0px auto;max-width:600px;">
+                  <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
+                      <tbody>
+                          <tr>
+                              <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:9px 0px 9px 0px;">
+                                  <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:600px;">      <![endif]-->
+                                  <div class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                                      <table role="presentation" cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                                          <tbody>
+                                              <tr>
+                                                  <td style="word-wrap:break-word;font-size:0px;padding:10px 10px 0px 12px;" align="left">
+                                                      <div style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:11px;line-height:1.5;text-align:left;">
+                                                          <h3 style="line-height: 100%;"><span style="font-size:16px; margin-bottom: 15px;">Hello,</span></h3>
+                                                          <h3 style="line-height: 100%;"><span style="font-size:20px; margin-bottom: 15px;">Thanks for joining <span style="color:#4caf50;">Revision</span> Check!</span></h3>
+                                                          <h3 style="line-height: 100%;"><span style="font-size:16px;">Please confirm your email address to activate your account.</span></h3></div>
+                                                  </td>
+                                              </tr>
+                                              <tr>
+                                                  <td style="word-wrap:break-word;font-size:0px;padding:12px 12px 12px 12px;" align="left">
+                                                      <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;width:auto;" align="left" border="0">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td style="border:0px solid #000;border-radius:5px;color:#fff;cursor:auto;padding:10px 30px;" align="center" valign="middle" bgcolor="#4CAF50"><a href="https://revisioncheck.com/verifylogin?confirmcode=` + user.confirmEmailCode + ` style="text-decoration:none;background:#4CAF50;color:#fff;font-family:Arial, sans-serif;font-size:15px;font-weight:normal;line-height:120%;text-transform:none;margin:0px;" target="_blank">Confirm my email</a></td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </div>
+                                  <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+                              </td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+              <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+          </div>
+      </body>
+
+      </html>
+
+
+      `;
+
+
+
+
+
+
+
+
+
       var text = "Welcome to <strong>Revision Check!</strong>\
           <br><br>\
           <p>Please click <a href='https://revisioncheck.com/verifylogin?confirmcode=" + user.confirmEmailCode + "'>here</a> to verify your account</p>\
