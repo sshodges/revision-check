fd32771f4f489ffc608f43ebe091b4396b93cc6a
sshodges <sshodges@gmail.com>
2019-02-13 11:46:02 +1100

    Update document.js

--- models/document.js
+++ models/document.js
@@ -24,7 +24,7 @@ module.exports = function (sequelize, DataTypes) {
           defaultValue: false
         },
         documentPassword: {
-          type: DataTypes.STRING,
+          type: DataTypes.STRING
         }
     });
 };
