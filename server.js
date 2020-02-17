91adeed44e6baf4e52b6cb8d44107126888f93a0
sshodges <sshodges@gmail.com>
2020-02-18 10:11:06 +1100

    Update server.js

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
-  
+  socket.on('join', function(room) {
+    console.log('Join', room);
+    socket.join(room);
+  });
 });
 
-io.on('join', function(room) {
-  console.log('Join', room);
-  io.join(room);
-});
+
 
 //GLOBAL VARS
 //Nodemailer Settings
